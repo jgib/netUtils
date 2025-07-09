9882cd72c64496576a854ca6b13b4be3982c332e
gibersonja <gibersonja@gmail.com>
2025-07-09 14:33:20 -0500

    starting fresh using my new debug window template

---
+++ utils/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using debug = netUtils.verbose;
+
+namespace netUtils
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            foreach (string arg in args)
+            {
+                if (arg == "-v" || arg == "--verbose")
+                {
+                    debug.CreateForm();
+                }
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                debug.Append($"TEST [{i}]");
+            }
+        }
+    }
+}
--- utils/Properties/AssemblyInfo.cs
+++ utils/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("netUtils")]
-[assembly: AssemblyDescription("Useful Network Utilities")]
+[assembly: AssemblyTitle("utils")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Skel")]
+[assembly: AssemblyProduct("utils")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0beecd88-51e5-4c5a-9450-f1f618266b56")]
+[assembly: Guid("467921b6-fdac-44e1-b97a-debf5a60fbdd")]
 
 // Version information for an assembly consists of the following four values:
 //
